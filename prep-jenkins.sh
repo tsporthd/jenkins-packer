d12706d9945c7fc1f4ad647e61fcd213b94877ca
Larry Presswood <larry.presswood@regions.com>
2019-05-03 14:15:53 -0400

    Change to add terraform, ansible etc

--- prep-jenkins.sh
+++ prep-jenkins.sh
@@ -9,7 +9,11 @@ sudo yum install wget unzip zip -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install jenkins java-1.8.0-openjdk -y
+# Install Packer
 sudo curl -o /usr/local/bin/packer.zip https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_linux_amd64.zip
 sudo unzip /usr/local/bin/packer.zip
+# Install Terraform
+sudo curl -o /usr/local/bin/terraform.zip  https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+sudo unzip /usr/local/bin/terraform.zip
 sudo service jenkins start
 sudo chkconfig jenkins on
